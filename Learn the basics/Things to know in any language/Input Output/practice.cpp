050d0c95e160019949888d202c559bc3f2b702d1
pk <infinityhacker105@gmail.com>
2026-06-30 22:02:14 +0545

    Basic input output of any program

---
+++ Learn the basics/Things to know in any language/Input Output/practice.cpp
@@ -0,0 +1,23 @@
+#include <iostream> // library use for input and output
+#include<bits/stdc++.h> //include almost all standard libraries at once 
+using namespace std; // Remove the need to use prefix std::
+
+int main(){
+    std::cout <<"hey prajwal";
+    std::cout <<"hey prajwal"; // printing on multiple lines
+    std::cout<<"hey prajwal"<< "\n"; //print on the new lines
+    std::cout<<"hey prajwal";
+    cout<<"hey prajwal"<<"\n";
+    
+    int x;
+    cin >> x; //used to take input from the user
+    cout <<"Value of x: " <<x <<"\n";
+
+    int a,b;
+    cin >> a >> b;
+    cout <<"Value of x: "<< a << " and y: "<< b;
+
+    return 0;
+};
+
+
